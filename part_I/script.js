667e0528c8711519e075bb1bc49b4463b2ab2d73
Armando Montiel <armtiel12@gmail.com>
2022-03-02 15:25:37 -0700

    first commit

---
+++ part_I/script.js
@@ -0,0 +1,7 @@
+var x = 3;
+
+function like(){
+    document.querySelector("#num").innerText = ++x;
+    // x++;
+}
+//var num = (don't need a variable you won't use)
---
+++ part_II/script.js
@@ -0,0 +1,15 @@
+var x = 9;
+var y = 12;
+var z = 9;
+
+function likeOne(){
+    document.querySelector("#neil").innerText = ++x;
+}
+
+function likeTwo(){
+    document.querySelector("#nichole").innerText = ++y;
+}
+
+function likeThree(){
+    document.querySelector("#jim").innerText = ++z;
+}
